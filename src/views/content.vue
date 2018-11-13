801b0e0eec60a4da6e576c37d7ee28b7e48c18f0
dhyjlas <dhyjlas@163.com>
2018-11-13 14:19:36 +0800

    增加正文字体大小调整

--- src/views/content.vue
+++ src/views/content.vue
@@ -5,20 +5,23 @@
         <a href="#" slot="extra" @click.prevent="drawer = true">
             <Icon type="ios-construct" />
         </a>
-        <div v-html="chapter.content"></div>
+        <div :style="'font-size: '+fontSize+'px'" v-html="chapter.content"></div>
         <div style="margin-top:10px;text-align:center;">
-            <a v-show="vlast" @click="last()">上一章</a>
+            <a v-show="!vlast" @click="last()">上一章</a>
             <a @click="goBook()" style="margin-left:80px;">返回目录</a>
-            <a v-show="vnext" @click="next()" style="margin-left:80px;">下一章</a>
+            <a v-show="!vnext" @click="next()" style="margin-left:80px;">下一章</a>
         </div>
         <Drawer :closable="false" v-model="drawer">
             <Button type="primary" @click="goIndex()" long>返回首页</Button>
             <Button type="primary" @click="refresh()" long style="margin-top:20px;" :loading="loading">刷新图书</Button>
             <Divider />
+            <Button type="primary" @click="increase()" long :disabled="vIncrease">增大字体</Button>
+            <Button type="primary" @click="reduce()" long :disabled="vReduce" style="margin-top:20px;">减小字体</Button>
+            <Divider />
             <Button type="primary" @click="again()" long>重载内容</Button>
             <Button type="primary" @click="goBook()" long style="margin-top:20px;">返回目录</Button>
-            <Button v-show="vlast" type="primary" @click="last()" long style="margin-top:20px;">上一章</Button>
-            <Button v-show="vnext" type="primary" @click="next()" long style="margin-top:20px;">下一章</Button>
+            <Button type="primary" @click="last()" long :disabled="vlast" style="margin-top:20px;">上一章</Button>
+            <Button type="primary" @click="next()" long :disabled="vnext" style="margin-top:20px;">下一章</Button>
         </Drawer>
         <Spin size="large" fix v-if="spinShow"></Spin>
     </Card>
@@ -30,10 +33,13 @@
 export default {
     data () {
         return {
+            fontSize: 14,
             spinShow: false,
             loading: false,
-            vlast:true,
-            vnext:true,
+            vlast: false,
+            vnext: false,
+            vIncrease: false,
+            vReduce: false,
             book: [],
             chapter: [],
             drawer: false,
@@ -55,6 +61,10 @@ export default {
             total: parseInt(window.localStorage.getItem("reading_chapter_total"))
         }
         this.setLastAndNext();
+        if(window.localStorage.getItem("reading_config_fontSize")){
+            this.fontSize = window.localStorage.getItem("reading_config_fontSize");
+        }
+        this.disabledFont();
     },
     methods: {
         goIndex(){
@@ -97,14 +107,14 @@ export default {
         },
         setLastAndNext(){
             if(this.chapter.serial <= 1){
-                this.vlast = false;
-            }else{
                 this.vlast = true;
+            }else{
+                this.vlast = false;
             }
             if(this.chapter.serial >= this.chapter.total){
-                this.vnext = false;
-            }else{
                 this.vnext = true;
+            }else{
+                this.vnext = false;
             }
             var size = parseInt(window.localStorage.getItem("reading_book_size_" + this.book.bookId));
             window.localStorage.setItem("reading_book_page_" + this.book.bookId, parseInt((this.chapter.serial - 1) / size))
@@ -146,6 +156,23 @@ export default {
         next(){
             this.spinShow = true;
             this.getContentByPage(this.chapter.serial + 1);
+        },
+        increase(){
+            this.fontSize++;
+            window.localStorage.setItem("reading_config_fontSize", this.fontSize);
+            this.disabledFont();
+        },
+        reduce(){
+            this.fontSize--;
+            window.localStorage.setItem("reading_config_fontSize", this.fontSize);
+            this.disabledFont();
+        },
+        disabledFont(){
+            if(this.fontSize <= 12){
+                this.vReduce = true;
+            }else{
+                this.vReduce = false;
+            }
         },
         again(){
             this.spinShow = true;
